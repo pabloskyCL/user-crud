80b93570e9d18d1a2fdc574a283ac35aa4a9ccad
Pablo Quiroz <pabloskiquiroz@gmail.com>
2024-06-24 21:11:22 -0400

    add login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\RegisterUserRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -16,7 +18,29 @@ class AuthController extends Controller
             $user = new User($data);
             $user->save();
 
-            return new JsonResponse(['success' => true, 'message' => 'usuario creado con exito'],JsonResponse::HTTP_CREATED);
+            return response()->json(['success' => true, 'message' => 'usuario creado con exito'], JsonResponse::HTTP_CREATED);
         }
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        $credentials = $request->validated();
+        // dd($credentials);
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'usuario o contraseña invalidos', ['error' => 'usuario o contraseña invalidos'],
+            ]);
+        }
+
+        $user = $request->user();
+
+        $token = $user->createToken('loginToken')->plainTextToken;
+
+        return response()->json([
+            'user' => [
+                'name' => $user->name,
+            ],
+            'accessToken' => $token,
+        ]);
     }
 }
